d2aaa4bc9620920d3a930fb393abe2e8c4799fa4
Liam McFadden <liam.e.mcfadden@gmail.com>
2022-07-28 20:30:43 -0700

    Use MUI and begin building site

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,44 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import * as React from "react";
+import type { AppProps } from "next/app";
+import { CacheProvider, EmotionCache } from "@emotion/react";
+import { ThemeProvider, CssBaseline, createTheme } from "@mui/material";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+import createEmotionCache from "../utility/createEmotionCache";
+import "../styles/globals.css";
+
+import { ThemeOptions } from "@mui/material/styles";
+
+const themeOptions: ThemeOptions = {
+  typography: {
+    fontFamily: "Poppins, sans-serif",
+    fontSize: 16,
+  },
+};
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
 }
 
-export default MyApp
+const clientSideEmotionCache = createEmotionCache();
+
+const theme = createTheme(themeOptions);
+
+const MyApp: React.FunctionComponent<MyAppProps> = (props) => {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
+};
+
+export default MyApp;
